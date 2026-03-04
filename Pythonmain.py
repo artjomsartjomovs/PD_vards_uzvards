a6eaa2dc62d47e6f6666df361914a48c71214a02
artjomsartjomovs <artjoms.artjomovs@gmail.com>
2026-03-04 14:36:57 +0200

    Uzrakstita funkcija aprekinu veiksanai

--- Pythonmain.py
+++ Pythonmain.py
@@ -3,6 +3,12 @@ D=float(input("Ievadi smilts daudzumu"))
 B=float(input("Ievadi cementa cena"))
 C=float(input("Ievadi smilts cena"))
 
+(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)
+(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)
+(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)(aprekini)
+smilts=3*A
+cena=smilts+0.5*C
+
 
 Smiltuden()
 Cena()
