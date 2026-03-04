23de51bec52143dcb80618ac3f92e936464fbb3e
artjomsartjomovs <artjoms.artjomovs@gmail.com>
2026-03-04 14:32:29 +0200

    uzrakstita funkcija datu ievadisana

--- Pythonmain.py
+++ Pythonmain.py
@@ -1,3 +1,9 @@
+A=float(input("Ievadi cementa daudzumu"))
+D=float(input("Ievadi smilts daudzumu"))
+B=float(input("Ievadi cementa cena"))
+C=float(input("Ievadi smilts cena"))
+
+
 Smiltuden()
 Cena()
 Galam()
